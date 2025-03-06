51c50d8f7f1d3aee825d2408906a93348a713b57
rgeilings <rene@ageos.nl>
2025-03-06 15:51:57 +0100

    generate AI images local with flux improvements

--- raspiBlog/scripts/maakBlogPost.py
+++ raspiBlog/scripts/maakBlogPost.py
@@ -1,6 +1,405 @@
 from raspiBlogLib import * # Importeer alle functies uit raspiBlogLib
 import base64
 
+# URL's voor GET en POST requests
+get_url = "http://192.168.178.76:7860/sdapi/v1/options"
+post_url = "http://192.168.178.76:7860/sdapi/v1/options"
+
+# Vooraf gekozen options
+chosen_options = {
+    "samples_save": True,
+    "samples_format": "png",
+    "samples_filename_pattern": "",
+    "save_images_add_number": True,
+    "save_images_replace_action": "Replace",
+    "grid_save": True,
+    "grid_format": "png",
+    "grid_extended_filename": False,
+    "grid_only_if_multiple": True,
+    "grid_prevent_empty_spots": False,
+    "grid_zip_filename_pattern": "",
+    "n_rows": -1.0,
+    "font": "",
+    "grid_text_active_color": "#000000",
+    "grid_text_inactive_color": "#999999",
+    "grid_background_color": "#ffffff",
+    "save_images_before_face_restoration": False,
+    "save_images_before_highres_fix": False,
+    "save_images_before_color_correction": False,
+    "save_mask": False,
+    "save_mask_composite": False,
+    "jpeg_quality": 80.0,
+    "webp_lossless": False,
+    "export_for_4chan": True,
+    "img_downscale_threshold": 4.0,
+    "target_side_length": 4000.0,
+    "img_max_size_mp": 200.0,
+    "use_original_name_batch": True,
+    "use_upscaler_name_as_suffix": False,
+    "save_selected_only": True,
+    "save_write_log_csv": True,
+    "save_init_img": False,
+    "temp_dir": "",
+    "clean_temp_dir_at_start": False,
+    "save_incomplete_images": False,
+    "notification_audio": True,
+    "notification_volume": 100.0,
+    "outdir_samples": "",
+    "outdir_txt2img_samples": "outputs/txt2img-images",
+    "outdir_img2img_samples": "outputs/img2img-images",
+    "outdir_extras_samples": "outputs/extras-images",
+    "outdir_grids": "",
+    "outdir_txt2img_grids": "outputs/txt2img-grids",
+    "outdir_img2img_grids": "outputs/img2img-grids",
+    "outdir_save": "log/images",
+    "outdir_init_images": "outputs/init-images",
+    "save_to_dirs": True,
+    "grid_save_to_dirs": True,
+    "use_save_to_dirs_for_ui": False,
+    "directories_filename_pattern": "[date]",
+    "directories_max_prompt_words": 8.0,
+    "ESRGAN_tile": 192.0,
+    "ESRGAN_tile_overlap": 8.0,
+    "realesrgan_enabled_models": [
+        "R-ESRGAN 4x+",
+        "R-ESRGAN 4x+ Anime6B"
+    ],
+    "dat_enabled_models": [
+        "DAT x2",
+        "DAT x3",
+        "DAT x4"
+    ],
+    "DAT_tile": 192.0,
+    "DAT_tile_overlap": 8.0,
+    "upscaler_for_img2img": None,
+    "set_scale_by_when_changing_upscaler": False,
+    "face_restoration": False,
+    "face_restoration_model": "CodeFormer",
+    "code_former_weight": 0.5,
+    "face_restoration_unload": False,
+    "auto_launch_browser": "Local",
+    "enable_console_prompts": False,
+    "show_warnings": False,
+    "show_gradio_deprecation_warnings": True,
+    "memmon_poll_rate": 8.0,
+    "samples_log_stdout": False,
+    "multiple_tqdm": True,
+    "enable_upscale_progressbar": True,
+    "print_hypernet_extra": False,
+    "list_hidden_files": True,
+    "disable_mmap_load_safetensors": False,
+    "hide_ldm_prints": True,
+    "dump_stacks_on_signal": False,
+    "profiling_explanation": "Those settings allow you to enable torch profiler when generating pictures.\nProfiling allows you to see which code uses how much of computer's resources during generation.\nEach generation writes its own profile to one file, overwriting previous.\nThe file can be viewed in <a href=\"chrome:tracing\">Chrome</a>, or on a <a href=\"https://ui.perfetto.dev/\">Perfetto</a> web site.\nWarning: writing profile can take a lot of time, up to 30 seconds, and the file itelf can be around 500MB in size.",
+    "profiling_enable": False,
+    "profiling_activities": [
+        "CPU"
+    ],
+    "profiling_record_shapes": True,
+    "profiling_profile_memory": True,
+    "profiling_with_stack": True,
+    "profiling_filename": "trace.json",
+    "api_enable_requests": True,
+    "api_forbid_local_requests": True,
+    "api_useragent": "",
+    "unload_models_when_training": False,
+    "pin_memory": False,
+    "save_optimizer_state": False,
+    "save_training_settings_to_txt": True,
+    "dataset_filename_word_regex": "",
+    "dataset_filename_join_string": " ",
+    "training_image_repeats_per_epoch": 1.0,
+    "training_write_csv_every": 500.0,
+    "training_xattention_optimizations": False,
+    "training_enable_tensorboard": False,
+    "training_tensorboard_save_images": False,
+    "training_tensorboard_flush_every": 120.0,
+    "sd_model_checkpoint": "flux1-schnell-fp8.safetensors",
+    "sd_checkpoints_limit": 1.0,
+    "sd_checkpoints_keep_in_cpu": True,
+    "sd_checkpoint_cache": 0,
+    "sd_unet": "Automatic",
+    "enable_quantization": False,
+    "emphasis": "Original",
+    "enable_batch_seeds": True,
+    "comma_padding_backtrack": 20.0,
+    "sdxl_clip_l_skip": False,
+    "CLIP_stop_at_last_layers": 1.0,
+    "upcast_attn": False,
+    "randn_source": "GPU",
+    "tiling": False,
+    "hires_fix_refiner_pass": "second pass",
+    "sdxl_crop_top": 0,
+    "sdxl_crop_left": 0,
+    "sdxl_refiner_low_aesthetic_score": 2.5,
+    "sdxl_refiner_high_aesthetic_score": 6.0,
+    "sd3_enable_t5": False,
+    "sd_vae_explanation": "<abbr title='Variational autoencoder'>VAE</abbr> is a neural network that transforms a standard <abbr title='red/green/blue'>RGB</abbr>\nimage into latent space representation and back. Latent space representation is what stable diffusion is working on during sampling\n(i.e. when the progress bar is between empty and full). For txt2img, VAE is used to create a resulting image after the sampling is finished.\nFor img2img, VAE is used to process user's input image before the sampling, and to create an image after sampling.",
+    "sd_vae_checkpoint_cache": 0,
+    "sd_vae": "sdxl_vae.safetensors",
+    "sd_vae_overrides_per_model_preferences": True,
+    "auto_vae_precision_bfloat16": False,
+    "auto_vae_precision": True,
+    "sd_vae_encode_method": "Full",
+    "sd_vae_decode_method": "Full",
+    "inpainting_mask_weight": 1.0,
+    "initial_noise_multiplier": 1.0,
+    "img2img_extra_noise": 0.0,
+    "img2img_color_correction": False,
+    "img2img_fix_steps": False,
+    "img2img_background_color": "#ffffff",
+    "img2img_sketch_default_brush_color": "#ffffff",
+    "img2img_inpaint_mask_brush_color": "#ffffff",
+    "img2img_inpaint_sketch_default_brush_color": "#ffffff",
+    "img2img_inpaint_mask_high_contrast": True,
+    "img2img_inpaint_mask_scribble_alpha": 75.0,
+    "return_mask": False,
+    "return_mask_composite": False,
+    "img2img_batch_show_results_limit": 32.0,
+    "overlay_inpaint": True,
+    "img2img_autosize": False,
+    "img2img_batch_use_original_name": False,
+    "cross_attention_optimization": "Automatic",
+    "s_min_uncond": 0.0,
+    "s_min_uncond_all": False,
+    "token_merging_ratio": 0.0,
+    "token_merging_ratio_img2img": 0.0,
+    "token_merging_ratio_hr": 0.0,
+    "pad_cond_uncond": False,
+    "pad_cond_uncond_v0": False,
+    "persistent_cond_cache": True,
+    "batch_cond_uncond": True,
+    "fp8_storage": "Disable",
+    "cache_fp16_weight": False,
+    "forge_try_reproduce": "None",
+    "auto_backcompat": True,
+    "use_old_emphasis_implementation": False,
+    "use_old_karras_scheduler_sigmas": False,
+    "no_dpmpp_sde_batch_determinism": False,
+    "use_old_hires_fix_width_height": False,
+    "hires_fix_use_firstpass_conds": False,
+    "use_old_scheduling": False,
+    "use_downcasted_alpha_bar": False,
+    "refiner_switch_by_sample_steps": False,
+    "interrogate_keep_models_in_memory": False,
+    "interrogate_return_ranks": False,
+    "interrogate_clip_num_beams": 1.0,
+    "interrogate_clip_min_length": 24.0,
+    "interrogate_clip_max_length": 48.0,
+    "interrogate_clip_dict_limit": 1500.0,
+    "interrogate_clip_skip_categories": [],
+    "interrogate_deepbooru_score_threshold": 0.5,
+    "deepbooru_sort_alpha": True,
+    "deepbooru_use_spaces": True,
+    "deepbooru_escape": True,
+    "deepbooru_filter_tags": "",
+    "extra_networks_show_hidden_directories": True,
+    "extra_networks_dir_button_function": False,
+    "extra_networks_hidden_models": "When searched",
+    "extra_networks_default_multiplier": 1.0,
+    "extra_networks_card_width": 0,
+    "extra_networks_card_height": 0,
+    "extra_networks_card_text_scale": 1.0,
+    "extra_networks_card_show_desc": True,
+    "extra_networks_card_description_is_html": False,
+    "extra_networks_card_order_field": "Path",
+    "extra_networks_card_order": "Ascending",
+    "extra_networks_tree_view_style": "Dirs",
+    "extra_networks_tree_view_default_enabled": True,
+    "extra_networks_tree_view_default_width": 180.0,
+    "extra_networks_add_text_separator": " ",
+    "ui_extra_networks_tab_reorder": "",
+    "textual_inversion_print_at_load": False,
+    "textual_inversion_add_hashes_to_infotext": True,
+    "sd_hypernetwork": "None",
+    "keyedit_precision_attention": 0.1,
+    "keyedit_precision_extra": 0.05,
+    "keyedit_delimiters": ".,\/!?%^*;:{}=`~() ",
+    "keyedit_delimiters_whitespace": [
+        "Tab",
+        "Carriage Return",
+        "Line Feed"
+    ],
+    "keyedit_move": True,
+    "disable_token_counters": False,
+    "include_styles_into_token_counters": True,
+    "return_grid": True,
+    "do_not_show_images": False,
+    "js_modal_lightbox": True,
+    "js_modal_lightbox_initially_zoomed": True,
+    "js_modal_lightbox_gamepad": False,
+    "js_modal_lightbox_gamepad_repeat": 250.0,
+    "sd_webui_modal_lightbox_icon_opacity": 1.0,
+    "sd_webui_modal_lightbox_toolbar_opacity": 0.9,
+    "gallery_height": "",
+    "open_dir_button_choice": "Subdirectory",
+    "hires_button_gallery_insert": False,
+    "compact_prompt_box": False,
+    "samplers_in_dropdown": True,
+    "dimensions_and_batch_together": True,
+    "sd_checkpoint_dropdown_use_short": False,
+    "hires_fix_show_sampler": False,
+    "hires_fix_show_prompts": False,
+    "txt2img_settings_accordion": False,
+    "img2img_settings_accordion": False,
+    "interrupt_after_current": True,
+    "localization": "None",
+    "quick_setting_list": [],
+    "ui_tab_order": [],
+    "hidden_tabs": [],
+    "tabs_without_quick_settings_bar": [
+        "Spaces"
+    ],
+    "ui_reorder_list": [],
+    "gradio_theme": "Default",
+    "gradio_themes_cache": True,
+    "show_progress_in_title": True,
+    "send_seed": True,
+    "send_size": True,
+    "enable_reloading_ui_scripts": False,
+    "infotext_explanation": "Infotext is what this software calls the text that contains generation parameters and can be used to generate the same picture again.\nIt is displayed in UI below the image. To use infotext, paste it into the prompt and click the ↙️ paste button.",
+    "enable_pnginfo": True,
+    "stealth_pnginfo_option": "Alpha",
+    "save_txt": False,
+    "add_model_name_to_info": True,
+    "add_model_hash_to_info": True,
+    "add_vae_name_to_info": True,
+    "add_vae_hash_to_info": True,
+    "add_user_name_to_info": False,
+    "add_version_to_infotext": True,
+    "disable_weights_auto_swap": True,
+    "infotext_skip_pasting": [],
+    "infotext_styles": "Apply if any",
+    "show_progressbar": True,
+    "live_previews_enable": True,
+    "live_previews_image_format": "png",
+    "show_progress_grid": True,
+    "show_progress_every_n_steps": 10.0,
+    "show_progress_type": "Approx NN",
+    "live_preview_allow_lowvram_full": False,
+    "live_preview_content": "Prompt",
+    "live_preview_refresh_period": 1000.0,
+    "live_preview_fast_interrupt": False,
+    "js_live_preview_in_modal_lightbox": False,
+    "prevent_screen_sleep_during_generation": True,
+    "hide_samplers": [],
+    "eta_ddim": 0.0,
+    "eta_ancestral": 1.0,
+    "ddim_discretize": "uniform",
+    "s_churn": 0.0,
+    "s_tmin": 0.0,
+    "s_tmax": 0.0,
+    "s_noise": 1.0,
+    "sigma_min": 0.0,
+    "sigma_max": 0.0,
+    "rho": 0.0,
+    "eta_noise_seed_delta": 0,
+    "always_discard_next_to_last_sigma": False,
+    "sgm_noise_multiplier": False,
+    "uni_pc_variant": "bh1",
+    "uni_pc_skip_type": "time_uniform",
+    "uni_pc_order": 3.0,
+    "uni_pc_lower_order_final": True,
+    "sd_noise_schedule": "Default",
+    "skip_early_cond": 0.0,
+    "beta_dist_alpha": 0.6,
+    "beta_dist_beta": 0.6,
+    "postprocessing_enable_in_main_ui": [],
+    "postprocessing_disable_in_extras": [],
+    "postprocessing_operation_order": [],
+    "upscaling_max_images_in_cache": 5.0,
+    "postprocessing_existing_caption_action": "Ignore",
+    "disabled_extensions": [],
+    "disable_all_extensions": "none",
+    "restore_config_state_file": "",
+    "sd_checkpoint_hash": "ead426278b49030e9da5df862994f25ce94ab2ee4df38b556ddddb3db093bf72",
+    "forge_unet_storage_dtype": "Automatic",
+    "forge_inference_memory": 1024.0,
+    "forge_async_loading": "Queue",
+    "forge_pin_shared_memory": "CPU",
+    "forge_preset": "flux",
+    "forge_additional_modules": [
+        "/app/webui-forge/webui/models/VAE/ae.safetensors",
+        "/app/webui-forge/webui/models/text_encoder/clip_l.safetensors",
+        "/app/webui-forge/webui/models/text_encoder/t5xxl_fp8_e4m3fn.safetensors"
+    ],
+    "forge_canvas_plain": False,
+    "forge_canvas_toolbar_always": False,
+    "enable_prompt_comments": True,
+    "sd_t2i_width": 512.0,
+    "sd_t2i_height": 640.0,
+    "sd_t2i_cfg": 7.0,
+    "sd_t2i_hr_cfg": 7.0,
+    "sd_i2i_width": 512.0,
+    "sd_i2i_height": 512.0,
+    "sd_i2i_cfg": 7.0,
+    "xl_t2i_width": 896.0,
+    "xl_t2i_height": 1152.0,
+    "xl_t2i_cfg": 5.0,
+    "xl_t2i_hr_cfg": 5.0,
+    "xl_i2i_width": 1024.0,
+    "xl_i2i_height": 1024.0,
+    "xl_i2i_cfg": 5.0,
+    "xl_GPU_MB": 23228.0,
+    "flux_t2i_width": 896.0,
+    "flux_t2i_height": 1152.0,
+    "flux_t2i_cfg": 1.0,
+    "flux_t2i_hr_cfg": 1.0,
+    "flux_t2i_d_cfg": 3.5,
+    "flux_t2i_hr_d_cfg": 3.5,
+    "flux_i2i_width": 1024.0,
+    "flux_i2i_height": 1024.0,
+    "flux_i2i_cfg": 1.0,
+    "flux_i2i_d_cfg": 3.5,
+    "flux_GPU_MB": 23228.0,
+    "settings_in_ui": "This page allows you to add some settings to the main interface of txt2img and img2img tabs.",
+    "extra_options_txt2img": [],
+    "extra_options_img2img": [],
+    "extra_options_cols": 1.0,
+    "extra_options_accordion": False
+}
+
+# Functie om JSON data op te halen
+def get_options():
+    response = requests.get(get_url)
+    return response.json()
+
+# Functie om JSON data te posten
+def post_options(options):
+    response = requests.post(post_url, json=options)
+    return response.status_code
+
+# Functie om de gekozen options te zetten
+def set_chosen_options():
+    current_options = get_options()
+    if current_options != chosen_options:
+        # Bewaar huidige options als backup
+        backup_options = current_options
+
+        # Post nieuwe options
+        post_status = post_options(chosen_options)
+        if post_status == 200:
+            print("Nieuwe options succesvol gepost.")
+            return backup_options
+        else:
+            print("Fout bij het posten van nieuwe options.")
+            return None
+    else:
+        print("Huidige options komen overeen met de vooraf gekozen options. Geen actie nodig.")
+        return None
+
+# Functie om de bewaarde options terug te zetten
+def restore_options(backup_options):
+    if backup_options:
+        restore_status = post_options(backup_options)
+        if restore_status == 200:
+            print("Oude options succesvol hersteld.")
+        else:
+            print("Fout bij het herstellen van oude options.")
+    else:
+        print("Geen backup options nodig om terug te zetten.")
+
+
 def generate_ai_image (prompt, output_filename):
     """
     Genereert een afbeelding op basis van de gegeven prompt via een API-aanroep.
@@ -135,6 +534,8 @@ def generate_title(NieuwsType):
 
 def main(NieuwsType):
     locale.setlocale(locale.LC_TIME, 'nl_NL.UTF-8')
+    # haal bestaande options op en bewaar deze. Zet daarna ge gewenste options
+    backup = set_chosen_options()
     # Mapping van NieuwsType naar view_names
     view_mapping = {
         "Recent": "rb_v_6_random_recent_articles",
@@ -195,6 +596,8 @@ def main(NieuwsType):
     else:
       print("BLOG_FILE is leeg")
 
+    # zet bewaarde options terug
+    backup = set_chosen_options()
     update_run_status(runid, AI_provider, 'C')
 
 if __name__ == "__main__":
